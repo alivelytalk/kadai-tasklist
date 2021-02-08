9f8d1df37bf9973f58edc1e31fd47928e479e1eb
c.m <hhashibosogarasu@gmail.com>
2021-02-08 17:16:47 +0000

    status

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_02_04_165648) do
+ActiveRecord::Schema.define(version: 2021_02_08_163338) do
 
   create_table "tasks", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "content"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "status"
   end
 
 end
